27f3a950a81835a8186654bd65872f1ece2c53bb
Natalia Gilyazova <gilyazovan97@mail.ru>
2022-06-24 16:51:42 +0300

    Created a header, promo and footer of the site. Implemented a slider in the promo block

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+};
+
+const prepareCurrentSlide = int => {
+    activeSlide(index);
+};
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+};
+
+/* setInterval(nextSlide, 6000); */
